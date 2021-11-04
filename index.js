33e8e5be6b59625065e16e1cee307fc613b54972
DevWeb13 <giuliano.loic@sfr.fr>
2021-11-04 16:46:28 +0100

    feat(userDisplay): Création dayCalc() => Nbr de jours depuis inscription

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ let userData = [];
 const fetchUser = async () => {
   await fetch("https://randomuser.me/api/?results=24")
     .then((res) => res.json())
-    .then((data) => (userData = data.results))
-    .then((userData) => console.log(userData));
+    .then((data) => (userData = data.results));
 };
 
 /**
@@ -38,15 +37,30 @@ const userDisplay = async () => {
     return newDate;
   };
 
+  /**
+   * Calcul le nombre de jours depuis l'inscription
+   *
+   * @param   {Date}  date  Date in ISO format
+   *
+   * @return  {number}        Nombre de jours depuis l'inscription
+   */
+  const dayCalc = (date) => {
+    let today = new Date(); // Date du jour au format JS
+    let todayTimestamp = Date.parse(today.toString()); // Date du jour au format timestamp
+    let timestamp = Date.parse(date.toString()); // Date d'inscription format timestamp
+
+    return Math.ceil((todayTimestamp - timestamp) / 8.64e7); // Soustraction => division 8.64e pour avoir le nombre de jours => Math.ceil() arrondi le nombre à l'entier supérieur
+  };
+
   document.body.innerHTML = userData
     .map(
       (user) =>
         `
         <div class="card">
             <img src=${user.picture.large} alt="photo de ${user.name.last}">
-            <h3>${user.name.first}<h3>
+            <h3>${user.name.first} ${user.name.last}<h3>
             <p>${user.location.city}, ${dateParser(user.dob.date)}</p>
-            <em>Membre depuis : ${user.registered.date} jours</em>
+            <em>Membre depuis : ${dayCalc(user.registered.date)} jours</em>
         </div>    
         `
     )
