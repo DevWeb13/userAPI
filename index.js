594c7eda582421512bec7b882fd639b5e45d9a9e
DevWeb13 <giuliano.loic@sfr.fr>
2021-11-04 15:02:21 +0100

    feat(userDisplay()): Affichage des cartes de chaque utilisateur

--- index.js
+++ index.js
@@ -2,22 +2,38 @@
 
 let userData = [];
 
+/**
+ * Asynchronous function. Retrieves each user's data and then stores it in a array
+ *
+ * @return  {Promise}  Data stored in the userData array
+ */
 const fetchUser = async () => {
   await fetch("https://randomuser.me/api/?results=24")
     .then((res) => res.json())
-    .then((data) => (userData = data.results));
-  console.log(userData);
+    .then((data) => (userData = data.results))
+    .then((userData) => console.log(userData));
 };
 
+/**
+ * Asynchronous function. Displaying each user's maps
+ *
+ * @return  {Promise}  Displaying each user's maps
+ */
 const userDisplay = async () => {
   await fetchUser();
-
-  document.body.innerHTML = userData.map(
-    (user) =>
-      `
-  <h3>${user.name.first}<h3>
-  `
-  );
+  document.body.innerHTML = userData
+    .map(
+      (user) =>
+        `
+        <div class="card">
+            <img src=${user.picture.large} alt="photo de ${user.name.last}">
+            <h3>${user.name.first}<h3>
+            <p>${user.location.city}, ${user.dob.date}</p>
+            <em>Membre depuis : ${user.registered.date} jours</em>
+        </div>    
+        `
+    )
+    .join("");
 };
 
 userDisplay();
